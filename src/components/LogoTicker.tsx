84dd0c5a51a7d54ea2d92a55e1424211872c5a93
Alexandre Angrignon <alex040892@gmail.com>
2024-07-18 09:43:56 +1200

    logo ticker

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,10 +1,13 @@
+'use client';
+
 import acmeLogo from '../assets/images/acme.png';
 import quantumLogo from '../assets/images/quantum.png';
 import echoLogo from '../assets/images/echo.png';
 import celestialLogo from '../assets/images/celestial.png';
 import pulseLogo from '../assets/images/pulse.png';
 import apexLogo from '../assets/images/apex.png';
-
+import Image from 'next/image';
+import styles from './LogoTicker.module.css';
 const images = [
   { src: acmeLogo, alt: 'Acme Logo' },
   { src: quantumLogo, alt: 'Quantum Logo' },
@@ -14,4 +17,24 @@ const images = [
   { src: apexLogo, alt: 'Apex Logo' },
 ];
 
-export const LogoTicker = () => {};
+export const LogoTicker = () => {
+  return (
+    <div className='bg-black text-white py-[72px] sm:py-24'>
+      <div className='container'>
+        <h2 className='text-xl text-center text-white/70'>
+          Trusted by the world's most innovative teams
+        </h2>
+        <div
+          className={`overflow-hidden mt-9 before:content-[''] before:absolute before:h-full after:h-full before:w-5 after:w-5 after:absolute after:content-[''] relative 
+          after:right-0 before:left-0 before:top-0 after:top-0 ${styles.logoTickerGradient}`}
+        >
+          <div className='flex gap-16'>
+            {images.map(({ src, alt }) => (
+              <Image src={src} alt={alt} className='flex-none h-8 w-auto' />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
