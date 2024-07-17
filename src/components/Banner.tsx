8155c879ee7ddb1a27d132e3dc8a6d269892e17b
Alexandre Angrignon <alex040892@gmail.com>
2024-07-17 13:32:49 +1200

    Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,18 @@
+import styles from './Banner.module.css';
+
 export const Banner = () => {
-  return null;
+  return (
+    <div className={`${styles.bannerHeader} py-3 text-center`}>
+      <div className='container'>
+        <p className='font-medium'>
+          <span className='hidden sm:inline'>
+            Introducing a completely redesigned interface -{' '}
+          </span>
+          <a href='#' className='underline underline-offset-2 font-medium '>
+            Explore the demo
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       container: {
-        padding: '1rem', 
+        padding: '1rem',
         center: true,
-      }
+      },
+      colors: {
+        'linear-pink': '#FCD6FF',
+        'linear-blue': '#29D8FF',
+        'linear-yellow': '#FFFD80',
+        'linear-dark-pink': '#F89ABF',
+      },
     },
   },
   plugins: [],
