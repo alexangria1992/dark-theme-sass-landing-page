47b19cbcf5362798c94b44ec343fad35d45a4119
Alexandre Angrignon <alex040892@gmail.com>
2024-07-22 15:48:36 +1200

    CTA animation

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,21 +1,44 @@
+'use client';
 import helixImage from '../assets/images/helix2.png';
 import emojiStarImage from '../assets/images/emojistar.png';
 import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 export const CallToAction = () => {
+  const containerRef = useRef<HTMLDivElement>();
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end end'],
+  });
+
+  useEffect(() => {
+    scrollYProgress.on('change', (value) => console.log('value'));
+  }, []);
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [40, -40]);
+
   return (
-    <div className='bg-black text-white py-[72px] sm:py-24 text-center overflow-x-hidden'>
+    <div
+      className='bg-black text-white py-[72px] sm:py-24 text-center overflow-x-hidden'
+      ref={containerRef}
+    >
       <div className='container max-w-xl relative'>
-        <Image
-          src={helixImage}
-          alt=''
-          className='sm:absolute sm:top-6 sm:left-[calc(100%+36px)] sm:block hidden'
-        />
-        <Image
-          src={emojiStarImage}
-          alt=''
-          className='sm:absolute sm:-top-[120px] sm:right-[calc(100%+24px)] sm:block hidden'
-        />
+        <motion.div style={{ translateY }}>
+          <Image
+            src={helixImage}
+            alt=''
+            className='sm:absolute sm:top-6 sm:left-[calc(100%+36px)] sm:block hidden'
+          />
+        </motion.div>
+        <motion.div style={{ translateY }}>
+          <Image
+            src={emojiStarImage}
+            alt=''
+            className='sm:absolute sm:-top-[120px] sm:right-[calc(100%+24px)] sm:block hidden'
+          />
+        </motion.div>
         <h2 className='font-bold text-5xl tracking-tighter sm:text-6xl '>
           Get instant access
         </h2>
